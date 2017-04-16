aa7d6a63553e590342f80f533f7f1ac85431acc1
Per Thykjaer Jensen <per@multimusen.dk>
2017-04-16 08:39:49 +0200

    form-exercise.php

---
+++ form-exercise.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * file: form-exercise.php
+ **/
+?>
+
+<?php include 'header.php'; ?>
+
+<!-- add HTML5 for the survey form here -->
+
+<?php // add the php here ... ?>
+
+<?php include 'footer.php'; ?>
